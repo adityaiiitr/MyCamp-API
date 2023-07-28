a5aa299f8296abfe72cbfc0ea1fbdc126068f92a
adityaiiitr <adityaiiitr@gmail.com>
2023-07-28 16:50:17 +0530

    Mongoose error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,7 @@ exports.getBootcamps = async (req,res,next) =>{
         const bootcamps = await Bootcamp.find();
         res.status(200).json({success:true,count:bootcamps.length,data: bootcamps})
     } catch(err){
-        res.status(400).json({success:false})
+        next(err)
     }
 }
 
@@ -27,8 +27,8 @@ exports.getBootcamp = async (req,res,next) =>{
         res.status(200).json({success:true,data: bootcamp})
     } catch(err){
         // res.status(400).json({success:false})
-        // next(err) //custom express error handler
-        next(new ErrorResponse(`Bootcamp Not Found with id of ${req.params.id}`,404 ))
+        next(err) //custom express error handler
+        // next(new ErrorResponse(`Bootcamp Not Found with id of ${req.params.id}`,404 ))
     }
 }
 
@@ -41,7 +41,7 @@ exports.createBootcamp = async (req,res,next) =>{
         const bootcamp = await Bootcamp.create(req.body)
         res.status(200).json({success:true,data:bootcamp})
     }catch(err) {
-        res.status(400).json({success:false})
+        next(err)
     }
     
 }
@@ -61,9 +61,8 @@ exports.updateBootcamp = async (req,res,next) =>{
         }
         res.status(200).json({success:true,data:bootcamp})
     }catch(err) {
-        res.status(400).json({success:false})
+        next(err)
     }
-    res.status(200).json({success:true,msg:`Update  Bootcamps ${req.params.id}`})
 }
 
 // @desc Delete bootcamp
@@ -74,11 +73,10 @@ exports.deleteBootcamp = async (req,res,next) =>{
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
         if(!bootcamp){
             //for correctly formatted id not in db
-            return res.status(400).json({success:false})
+            return next(new ErrorResponse(`Bootcamp Not Found with id of ${req.params.id}`,404 ))
         }
         res.status(200).json({success:true,data:{}})
     }catch(err) {
-        res.status(400).json({success:false})
+        next(err)
     }
-    res.status(200).json({success:true,msg:`Update  Bootcamps ${req.params.id}`})
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,12 +1,33 @@
+const ErrorResponse = require('../utils/errorResponse')
 // Custom Error Middleware
-
-
 const errorHandler = (err,req,res,next) =>{
+    let error = {...err}
+
+    error.message = err.message
     // Log to console for dev
-    console.log(err.stack.red)
-    res.status(err.statusCode || 500).json({
+    console.log(err.red)
+
+    // Mongoose Bad ObjectId
+    if(err.name==='CastError') {
+        const message = `Bootcamp Not Found with id of ${err.value}`
+        error = new ErrorResponse(message,404)
+    }
+
+    // Mongoose Duplicate key
+    if(err.code===11000){
+        const message = 'Duplicate Field Value Entered'
+        error = new ErrorResponse(message,400)
+    }
+
+    // Mongoose Validation Error
+    if(err.name==='ValidationError') {
+        const message = Object.values(err.errors).map(val => " "+val.message)
+        error = new ErrorResponse(message,400)
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     })
 }
 
