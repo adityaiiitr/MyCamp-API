b964e4b3b7f597977513aa50bb04312d6fe72267
adityaiiitr <adityaiiitr@gmail.com>
2023-07-29 22:58:10 +0530

    mongoose middleware node-geocoder

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-// const geocoder = require('../utils/geocoder');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -117,24 +117,24 @@ BootcampSchema.pre('save', function(next) {
   next();
 });
 
-// // Geocode & create location field
-// BootcampSchema.pre('save', async function(next) {
-//   const loc = await geocoder.geocode(this.address);
-//   this.location = {
-//     type: 'Point',
-//     coordinates: [loc[0].longitude, loc[0].latitude],
-//     formattedAddress: loc[0].formattedAddress,
-//     street: loc[0].streetName,
-//     city: loc[0].city,
-//     state: loc[0].stateCode,
-//     zipcode: loc[0].zipcode,
-//     country: loc[0].countryCode
-//   };
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
 
-  // // Do not save address in DB
-  // this.address = undefined;
-  // next();
-// });
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
 
 // // Cascade delete courses when a bootcamp is deleted
 // BootcampSchema.pre('remove', async function(next) {
