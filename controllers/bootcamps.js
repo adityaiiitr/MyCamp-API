c451e808babf7a865c1895ea37c4bae97c7249f8
adityaiiitr <adityaiiitr@gmail.com>
2023-07-30 14:16:26 +0530

    Advanced filtering with $ in db query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,13 @@ const geocoder = require('../utils/geocoder')
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req,res,next) =>{
-        const bootcamps = await Bootcamp.find();
+        console.log(req.query)
+        let query;
+        let queryStr = JSON.stringify(req.query)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$${match}`)
+        console.log(queryStr)
+        query = Bootcamp.find(JSON.parse(queryStr));
+        const bootcamps = await query
         res.status(200).json({success:true,count:bootcamps.length,data: bootcamps})
 
 })
