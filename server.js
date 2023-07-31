fafc8aac311d122171aab7ce538b1eb57d679c77
adityaiiitr <adityaiiitr@gmail.com>
2023-07-31 21:30:16 +0530

    course controller and route added

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB()
 
 // routes files
 const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
 
 
 const app = express();
@@ -33,6 +34,7 @@ if(process.env.NODE_ENV === 'development'){
 
 // Mount routers
 app.use('/api/v1/bootcamps',bootcamps)
+app.use('/api/v1/courses',courses)
 
 // Error handler
 app.use(errorHandler)
