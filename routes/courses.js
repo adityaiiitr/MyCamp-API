c09b0d5371ad6ae4d49cba32c7aab89497c1d418
adityaiiitr <adityaiiitr@gmail.com>
2023-08-01 20:52:07 +0530

    Courses CRUD completed

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const {
-    getCourses
+    getCourses,
+    getCourse,
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses')
 
 const router = express.Router({mergeParams:true})
 
-router.route('/').get(getCourses)
+router.route('/').get(getCourses).post(addCourse)
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
 
 
 module.exports = router
