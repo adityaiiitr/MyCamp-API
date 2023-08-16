5dd8fe2910ec37143a46b8f2bab8a37434ba1182
adityaiiitr <adityaiiitr@gmail.com>
2023-08-16 13:08:39 +0530

    login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,3 +23,33 @@ exports.register = asyncHandler( async (req,res,next) => {
 
 })
 
+// @desc LogIn User
+// @route POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler( async (req,res,next) => {
+    const {email, password} = req.body;
+
+    // Validate email & password
+    if(!email || !password) {
+        return next(new ErrorResponse(`Please provide email and password`,400))
+    }
+
+    // Check for user
+    const user = await User.findOne({email}).select('+password')
+
+    if(!user){
+        return next(new ErrorResponse(`Invalid Credentials`,401))
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password)
+    if(!isMatch) {
+        return next(new ErrorResponse(`Invalid Credentials`,401))    
+    }
+
+    // Create a token 
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({success:true, token})
+
+})
