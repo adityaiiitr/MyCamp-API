666227c008a3ebf51ccd4694fb8e82937417157e
adityaiiitr <adityaiiitr@gmail.com>
2023-08-17 19:10:58 +0530

    get token response function

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler( async (req,res,next) => {
         role
     })
 
-    // Create a token 
-    const token = user.getSignedJwtToken()
-
-    res.status(200).json({success:true, token})
+    sendTokenResponse(user,200,res)
 
 })
 
@@ -47,10 +44,8 @@ exports.login = asyncHandler( async (req,res,next) => {
         return next(new ErrorResponse(`Invalid Credentials`,401))    
     }
 
-    // Create a token 
-    const token = user.getSignedJwtToken()
 
-    res.status(200).json({success:true, token})
+    sendTokenResponse(user,200,res)
 
 })
 
@@ -65,3 +60,26 @@ exports.getMe = asyncHandler( async (req,res,next) => {
         data:user
     })
 })
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+  
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+      ),
+      httpOnly: true,
+    };
+  
+    if (process.env.NODE_ENV === 'production') {
+      options.secure = true;
+    }
+  
+    res.status(statusCode).cookie('token', token, options).json({
+      success: true,
+      token,
+    });
+  };
+  
