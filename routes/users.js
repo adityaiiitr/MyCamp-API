ffa1428b476d2717b0a90ef634f43073ac12d3a6
adityaiiitr <adityaiiitr@gmail.com>
2023-08-17 23:55:32 +0530

    admin users crud

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const {
+    getUsers, getUser, updateUser, deleteUser, createUser
+} = require('../controllers/users')
+
+const advancedResults = require('../middleware/advancedResults')
+const User = require('../models/Users')
+
+const router = express.Router({mergeParams:true})
+
+const { protect, authorize } = require('../middleware/auth')
+
+router.use(protect);
+router.use(authorize('admin'));
+
+router
+  .route('/')
+  .get(advancedResults(User), getUsers)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
