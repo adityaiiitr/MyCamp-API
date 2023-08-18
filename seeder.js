c89af8e043cda9c138ea93a81bc8b140059cadc1
adityaiiitr <adityaiiitr@gmail.com>
2023-08-18 09:54:58 +0530

    get all reviews model and seeder

--- seeder.js
+++ seeder.js
@@ -10,7 +10,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/Users');
-// const Review = require('./models/Review');
+const Review = require('./models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -28,9 +28,9 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
-// const reviews = JSON.parse(
-//   fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
-// );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
@@ -38,7 +38,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
-    // await Review.create(reviews);
+    await Review.create(reviews);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -52,7 +52,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
-    // await Review.deleteMany();
+    await Review.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
@@ -65,13 +65,13 @@ const refreshData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
-    // await Review.deleteMany();
+    await Review.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     try {
       await Bootcamp.create(bootcamps);
       await Course.create(courses);
       await User.create(users);
-      // await Review.create(reviews);
+      await Review.create(reviews);
       console.log('Data Imported...'.green.inverse);
       process.exit();
     } catch (err) {
